0f971ad43278b615bcb7cddcb40bef3d5b974132
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-10-29 09:21:20 +0100

    Add smiley components and icons; enhance Intro section layout and styles

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,4 +1,7 @@
 import styles from './Intro.module.css';
+import sadSmiley from "../../assets/icons/sad.png";
+import happySmiley from "../../assets/icons/happy.png";
+import line from "../../assets/icons/graphLine.png";
 
 const Intro = () => {
   return (
@@ -7,6 +10,21 @@ const Intro = () => {
       <p className={styles.explanation}>
         We’ll explain everything in plain English. No jargon, no hidden costs.
       </p>
+      <section className={styles.smileyInfo}>
+        <div className={`${styles.smileyLeft} ${styles.smileys}`}>
+          <img src={sadSmiley} alt="Sad Smiley" />
+          <span>
+            <img src={line} alt="Graph Line" />
+          </span>
+        </div>
+        <div className={`${styles.smileyRight} ${styles.smileys}`}>
+          <img src={happySmiley} alt="Happy Smiley" />
+          <span>
+            <p className={styles.percent}>0.3%</p>
+            <p className={styles.clear}>Clear</p>
+          </span>
+        </div>
+      </section>
     </div>
   );
 }
