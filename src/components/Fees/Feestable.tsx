88a7e42cf65ac350990f371bce5ea4edadd48d8a
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-10-29 15:52:34 +0100

    change

--- src/components/Fees/Feestable.tsx
+++ src/components/Fees/Feestable.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./FeesTable.module.css";
+import styles from "./Feestable.module.css";
 
 type FeeItem = {
   Service: string;
